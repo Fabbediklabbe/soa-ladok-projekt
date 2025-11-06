d5d808b0e2fe211fdacadac905acd58faecaf7e6
Fabbediklabbe <blabbe.ny@gmail.com>
2025-11-06 14:03:54 +0100

    Lagt till mer exempeldata i dataseeder

--- src/main/java/se/ltu/studieresultat/seed/DataSeeder.java
+++ src/main/java/se/ltu/studieresultat/seed/DataSeeder.java
@@ -15,9 +15,25 @@ public class DataSeeder implements CommandLineRunner {
   private final StudentRepository stuRepo;
 
   @Override public void run(String... args) {
+
+    // Kurs D0031N SOA
     modRepo.save(new ModuleEntity(null, "D0031N", "0005", "Inlämningsuppgift", true));
     modRepo.save(new ModuleEntity(null, "D0031N", "0006", "Tentamen", true));
+
+    // Kurs D0025E Data Mining
+    modRepo.save(new ModuleEntity(null, "D0025E", "0005", "Inlämningsuppgift", true));
+    modRepo.save(new ModuleEntity(null, "D0025E", "0006", "Tentamen", true));
+
+    // Kurs D0005N Databaser II
+    modRepo.save(new ModuleEntity(null, "D0005N", "0005", "Inlämningsuppgift", true));
+    modRepo.save(new ModuleEntity(null, "D0005N", "0006", "Tentamen", true));
+
+
+    // Studenter
     stuRepo.save(new StudentEntity(null, "fabnys-3", "20020921-4321"));
     stuRepo.save(new StudentEntity(null, "axevik-3", "19992912-1234"));
+    stuRepo.save(new StudentEntity(null, "jonhje-2", "20010415-5678"));
+    stuRepo.save(new StudentEntity(null, "petvik-5", "19981130-7890"));
+    stuRepo.save(new StudentEntity(null, "matohv-7", "19970707-4567"));
   }
 }
